0b822564856c83867d292c196655e1d1f6da4cc0
jstudenski <jstudenski@gmail.com>
2015-03-10 18:41:15 -0500

    clock working

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
  
 $(document).ready(function () {
 
-  var h=today.getHours();
-  var m=today.getMinutes();
-  var s=today.getSeconds();
+  $("body").append("<p>test</p>");
 
-  $("body").append("<p>I\'m a paragraph!</p>");
-  
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function () {
+        startTime()
+    }, 500);
+}
+startTime();
+
+
 });
-
-        // function startTime()
-        // {
-        // var today=new Date();
-        // var h=today.getHours();
-        // var m=today.getMinutes();
-        // var s=today.getSeconds();
-         
-        // // add a zero in front of numbers<10
-        // m=checkTime(m);
-        // s=checkTime(s);
-        // if(h<12)
-        // document.getElementById('clock').innerHTML=h+":"+m+":"+s + " AM";
-        // else
-        // document.getElementById('clock').innerHTML=h+":"+m+":"+s + " PM";
- 
-        // t=setTimeout('startTime()',500);
-        // }
- 
-        // function checkTime(i)
-        // {
-        // if (i<10)
-        //   {
-        //   i="0" + i;
-        //   }
-        // return i;
-        // }
