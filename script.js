e0887609af8bb7162dda90064cd62a9f1318bfe9
jstudenski <jstudenski@gmail.com>
2015-03-10 18:28:40 -0500

    jQuery append body

---
+++ script.js
@@ -0,0 +1,37 @@
+ 
+$(document).ready(function () {
+
+  var h=today.getHours();
+  var m=today.getMinutes();
+  var s=today.getSeconds();
+
+  $("body").append("<p>I\'m a paragraph!</p>");
+  
+});
+
+        // function startTime()
+        // {
+        // var today=new Date();
+        // var h=today.getHours();
+        // var m=today.getMinutes();
+        // var s=today.getSeconds();
+         
+        // // add a zero in front of numbers<10
+        // m=checkTime(m);
+        // s=checkTime(s);
+        // if(h<12)
+        // document.getElementById('clock').innerHTML=h+":"+m+":"+s + " AM";
+        // else
+        // document.getElementById('clock').innerHTML=h+":"+m+":"+s + " PM";
+ 
+        // t=setTimeout('startTime()',500);
+        // }
+ 
+        // function checkTime(i)
+        // {
+        // if (i<10)
+        //   {
+        //   i="0" + i;
+        //   }
+        // return i;
+        // }
