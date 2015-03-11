9829524e62ec9aa60c525e4e38ba307e57e99c9e
jstudenski <jstudenski@gmail.com>
2015-03-10 19:36:05 -0500

    word number array created

--- test.js
+++ test.js
@@ -6,38 +6,11 @@ function round(x) {
 
 	var rounded = 5 * Math.round(x/5)
 
-	if (rounded == 0) {
-
-	} else if (rounded == 5) {
-		
-	} else if (rounded == 10) {
-
-	} else if (rounded == 15) {
-
-	} else if (rounded == 20) {
-
-	} else if (rounded == 25) {
-
-	} else if (rounded == 30) {
-
-	} else if (rounded == 35) {
-
-	} else if (rounded == 40) {
-
-	} else if (rounded == 45) {
-
-	} else if (rounded == 50) {
-
-	} else if (rounded == 55) {
-
-	} else if (rounded == 60) {
-
-	} else {
-		console.log("invalid number")
-	}
-
 }
 
 
 
 console.log(round(a) + "," + round(b) + "," +round(c));
+
+
+numberarr = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve"]
