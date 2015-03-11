ae1c5a51b3cfcb76787c19ad5411b7681e91167f
jstudenski <jstudenski@gmail.com>
2015-03-10 19:14:42 -0500

    if/else minute logic test

--- script.js
+++ script.js
@@ -18,15 +18,15 @@ function startTime() {
     // add a zero in front of numbers<10
     m = checkTime(m);
     s = checkTime(s);
+
     document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+
     t = setTimeout(function () {
         startTime()
     }, 500);
 }
 
 startTime();
-
-
 
 
 });
--- test.js
+++ test.js
@@ -4,11 +4,39 @@ var c = 13
 
 function round(x) {
 
-	return 5 * Math.round(x/5)
+	var rounded = 5 * Math.round(x/5)
+
+	if (rounded == 0) {
+
+	} else if (rounded == 5) {
+		
+	} else if (rounded == 10) {
+
+	} else if (rounded == 15) {
+
+	} else if (rounded == 20) {
+
+	} else if (rounded == 25) {
+
+	} else if (rounded == 30) {
+
+	} else if (rounded == 35) {
+
+	} else if (rounded == 40) {
+
+	} else if (rounded == 45) {
+
+	} else if (rounded == 50) {
+
+	} else if (rounded == 55) {
+
+	} else if (rounded == 60) {
+
+	} else {
+		console.log("invalid number")
+	}
 
 }
-
-
 
 
 
