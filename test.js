320c1561ce80a379f6a86d483379a4cb6ea2249d
jstudenski <jstudenski@gmail.com>
2015-03-10 19:42:16 -0500

    number logic AM hours

--- test.js
+++ test.js
@@ -4,13 +4,23 @@ var c = 13
 
 function round(x) {
 
-	var rounded = 5 * Math.round(x/5)
+	var rounded = 5 * Math.round(x/5);
+
+}
+
+function number_name(x) {
+
+	numberarr = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve"];
+
+	return numberarr[x - 1];
 
 }
 
 
 
-console.log(round(a) + "," + round(b) + "," +round(c));
 
 
-numberarr = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve"]
+
+
+console.log(number_name(3));
+
